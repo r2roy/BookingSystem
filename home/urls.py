c9c0445a21cbb9076168029ef3bd470334a8fad6
r2roy <prashantrai557@gmail.com>
2024-04-23 22:33:35 +0545

    login

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,9 @@
 from .views  import *
-from django.urls import path
+from django.urls import path,include
 
 
 urlpatterns =[
     path("",HomeView.as_view(), name="home"),
+    path("signup", signup, name="signup"),
+    path("accounts/", include('django.contrib.auth.urls')),
 ]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,8 @@
 from django.views import View
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import *
+from django.contrib import messages
+from django.contrib.auth.models import User
 
 # Create your views here.
 class BaseView(View):
@@ -19,5 +21,30 @@ class HomeView(BaseView):
         return render(request, 'index.html',self.views)
 
 
-
-
+def signup(request):
+    if request.method == 'POST':
+        fname = request.POST['first_name']
+        lname = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        cpassword = request.POST['cpassword']
+        if password == cpassword:
+            if User.objects.filter(username = username).exists():
+                messages.error(request, "Username already exists!")
+                return redirect('/sighup')
+            elif User.objects.filter(email = email).exists():
+                messages.error(request, "Email already in use!")
+                return redirect('/sighup')
+            else:
+                User.objects.create_user(
+                    first_name = fname,
+                    last_name = lname,
+                    username = username,
+                    email = email,
+                    password = password
+                ).save()
+        else:
+            messages.error(request, "Password does not match!")
+            return redirect('/signup')
+    return render(request,'signup.html')
