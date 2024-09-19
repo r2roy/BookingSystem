dcd3f4fb694de089dd71660b6b891f9fdbcdc019
r2roy <prashantrai557@gmail.com>
2024-09-19 14:39:26 +0545

    mail

--- booking/settings.py
+++ booking/settings.py
@@ -139,5 +139,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'prashantrai557@gmail.com'
+EMAIL_HOST_USER = 'example@gmail.com'
 EMAIL_HOST_PASSWORD = 'zwrx csei rrhf juyb'
